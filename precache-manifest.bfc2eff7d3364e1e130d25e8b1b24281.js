aa13d4f584f3f41b6889512f9c9590bcacdf423a
OmSingh <omsingh6903@gmail.com>
2020-06-09 23:31:39 +0530

    Deploy

---
+++ precache-manifest.bfc2eff7d3364e1e130d25e8b1b24281.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbed0efa424d2280b9abbab06f693aaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c238d711a1b182efcaa5",
+    "url": "/static/js/2.d96d70d6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d96d70d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5157038a32238c5deb6",
+    "url": "/static/js/main.96a6f6e4.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  }
+]);
